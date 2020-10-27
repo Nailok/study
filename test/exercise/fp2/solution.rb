f73cbfa68704dada9c92bf74413ff610cafca546
Nailok <lexitor34@gmail.com>
2020-10-27 20:12:55 +0400

    Rubocop fixes

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,7 +5,7 @@ module Exercise
 
       def my_each
         i = 0
-        while i < self.length
+        while i < length
           yield(self[i])
           i += 1
         end
@@ -14,31 +14,31 @@ module Exercise
       end
 
       def my_map
-        result = MyArray.new
-        self.my_reduce(result) { |result, item| result << yield(item) }
+        result_arr = MyArray.new
+        my_reduce(result_arr) { |result, item| result << yield(item) }
       end
-      
+
       def my_compact
         result = MyArray.new
-        self.my_each { |item| result << item unless item.nil? }
+        my_each { |item| result << item unless item.nil? }
 
         result
       end
 
-      def my_reduce(start_value=nil, &block)
-        temp_arr = self.dup
+      def my_reduce(start_value = nil, &block)
+        temp_arr = dup
         start_value = temp_arr.shift if start_value.nil?
 
         my_reduce_req(start_value, temp_arr, &block)
       end
 
-
-      private 
+      private
 
       def my_reduce_req(accumulator, arr, &block)
         return accumulator if arr.empty?
+
         accumulator = yield(accumulator, arr.first)
-        
+
         arr.shift
         my_reduce_req(accumulator, arr, &block)
       end
